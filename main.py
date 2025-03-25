0482a2f36036f364f5934c8de9f5582a0dd135c7
qinnzer <artpanchuk98@gmail.com>
2025-03-25 21:14:53 +0900

    add login, register. update css

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/main')
 def index():
     return render_template("index.html")
 
